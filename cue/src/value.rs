0a7c2dbb8fdd238190dfc11ce7e2f7a8637ea5de
Aram Hăvărneanu <aram@mgk.ro>
2024-08-01 19:21:14 +0200

    cue: add Value.to_json
    Also implement fmt::{Display, Debug} for Value,
    currently using to_json.
    
    Updates cue-lang/cue#3093.
    
    Change-Id: Ib0ceea4c7904a038ba0fab02a21d3f8b51e009c8

--- cue/src/value.rs
+++ cue/src/value.rs
@@ -1,6 +1,10 @@
+use std::fmt::{Debug, Display, Formatter, Result};
+use std::ptr;
+use std::slice;
+
 use cue_sys;
 
-use crate::Context;
+use crate::{Context, Error};
 
 #[derive(Debug)]
 pub struct Value {
@@ -17,10 +21,53 @@ impl Value {
     }
 }
 
+impl Value {
+    fn to_json(&self) -> String {
+        let mut buf_ptr = ptr::null_mut();
+        let mut len: usize = 0;
+
+        unsafe {
+            let err = cue_sys::cue_dec_json(self.res, &mut buf_ptr, &mut len);
+            if err != 0 {
+                return Error::from_res(err).to_string()
+            }
+
+            let slice = slice::from_raw_parts(buf_ptr as *const u8, len);
+            let s = String::from_utf8_lossy(slice).into_owned();
+            cue_sys::libc_free(buf_ptr);
+
+            s
+        }
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // TODO: print CUE syntax, not JSON.
+        write!(f, "{}", self.to_json())
+    }
+}
+
 impl Drop for Value {
     fn drop(&mut self) {
         unsafe {
             cue_sys::cue_free(self.res);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn to_json() {
+        let ctx = Context::new();
+
+        let v = crate::compile(&ctx, "1");
+        assert_eq!(v.unwrap().to_json(), "1");
+
+        let v = crate::compile(&ctx, "{ x: 1 }");
+        assert_eq!(v.unwrap().to_json(), "{\"x\":1}");
     }
 }
